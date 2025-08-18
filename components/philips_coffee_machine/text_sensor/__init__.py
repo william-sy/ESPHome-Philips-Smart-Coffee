e590e6c197c5f5b163f942acb54671ecc2c2da1c
William-niq <william.vanbeek@nielseniq.com>
2025-08-18 15:41:15 +0200

    Reverting to warnings this is fine

--- components/philips_coffee_machine/text_sensor/__init__.py
+++ components/philips_coffee_machine/text_sensor/__init__.py
@@ -12,12 +12,13 @@ StatusSensor = philips_status_sensor_ns.class_(
     "StatusSensor", text_sensor.TextSensor, cg.Component
 )
 
-CONFIG_SCHEMA = text_sensor.text_sensor_schema(
+CONFIG_SCHEMA = text_sensor.TEXT_SENSOR_SCHEMA.extend(
     {
         cv.GenerateID(): cv.declare_id(StatusSensor),
         cv.Required(CONTROLLER_ID): cv.use_id(PhilipsCoffeeMachine),
     }
-)
+).extend(cv.COMPONENT_SCHEMA)
+
 
 async def to_code(config):
     parent = await cg.get_variable(config[CONTROLLER_ID])
