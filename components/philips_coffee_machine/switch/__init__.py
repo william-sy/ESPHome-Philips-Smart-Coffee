53f46a1431538098632582ca7725f249ac9c2fbd
William-niq <william.vanbeek@nielseniq.com>
2025-11-24 15:15:45 +0100

    2025.11 bugfixes

--- components/philips_coffee_machine/switch/__init__.py
+++ components/philips_coffee_machine/switch/__init__.py
@@ -12,20 +12,20 @@ CLEAN_DURING_START = "clean"
 power_switch_namespace = philips_coffee_machine_ns.namespace("philips_power_switch")
 PowerSwitch = power_switch_namespace.class_("Power", switch.Switch, cg.Component)
 
-CONFIG_SCHEMA = switch.SWITCH_SCHEMA.extend(
+CONFIG_SCHEMA = switch.switch_schema(
+    PowerSwitch,
+).extend(
     {
-        cv.GenerateID(): cv.declare_id(PowerSwitch),
         cv.Required(CONTROLLER_ID): cv.use_id(PhilipsCoffeeMachine),
         cv.Optional(CLEAN_DURING_START, default=True): cv.boolean,
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
 
-def to_code(config):
-    controller = yield cg.get_variable(config[CONTROLLER_ID])
-    var = cg.new_Pvariable(config[CONF_ID])
-    yield cg.register_component(var, config)
-    yield switch.register_switch(var, config)
+async def to_code(config):
+    controller = await cg.get_variable(config[CONTROLLER_ID])
+    var = await switch.new_switch(config)
+    await cg.register_component(var, config)
 
     cg.add(var.set_cleaning(config[CLEAN_DURING_START]))
     cg.add(controller.register_power_switch(var))
